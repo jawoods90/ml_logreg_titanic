e178501cb3138dd453bec6db074fe8a3282165f3
JoeWoods <jw635.jw@gmail.com>
2024-01-27 18:09:25 +0000

    further descriptive stats on survival rates

--- titanic_training.py
+++ titanic_training.py
@@ -15,10 +15,12 @@ import matplotlib.pyplot as plt
 
 # Bring in dataset using seaborn
 df = sns.load_dataset("titanic")
+df = pd.DataFrame(df)
 
 
 # List of columns in the console, and then look at totals within some variables
 df.columns
+df.dtypes
 df['embark_town'].value_counts()
 df['sex'].value_counts()
 df['pclass'].value_counts()
@@ -29,5 +31,13 @@ tester = df[df['alive']=='yes']
 
 # calculate some quick survival rates by class and sex
 
-rsurvival_class = 
-[df['alive']=='yes'].sum()
+df['flag_svfemale'] = (df['sex']=='female')*df['survived']
+df['flag_svfemale'].value_counts()
+
+df['flag_svmale'] = (df['sex']=='male')*df['survived']
+df['flag_svmale'].value_counts()
+
+p_survive_female = 233 / 314 
+p_survive_male = 109 / 577
+
+# P 
